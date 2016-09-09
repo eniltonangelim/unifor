c5402d54e7be2a7644b18ef783c328543e488eb3
Enilton Angelim <enilton.angelim@gmail.com>
2016-09-09 12:01:50 -0300

    Validador de email

--- ProjetoAplicado/exemplo-angular2/app/form-aluno-component.ts
+++ ProjetoAplicado/exemplo-angular2/app/form-aluno-component.ts
@@ -1,26 +1,111 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
+import {ControlGroup, FormBuilder, Validators, AbstractControl} from 'angular2/common';
+import {EmailValidator} from './validators/email-validator';
 import {Curso} from './model/curso';
 import {Aluno} from './model/aluno';
+import {AlunoService} from './service/aluno-service';
 
 @Component({
     selector: 'form-aluno',
-    templateUrl: 'app/view/form-aluno.html'
+    templateUrl: 'app/view/form-aluno.html',
+    providers: [AlunoService]
 })
-export class FormAlunoComponent{
-    cursos: Curso[];
-    aluno: Aluno;     
 
+export class FormAlunoComponent implements OnInit {
+
+    cursos: Curso[];
     sucesso: boolean = false;
+    idEditar: number;
+    aluno: Aluno;     
+    alunoForm: ControlGroup;
+    alunos: Aluno[];
+
+    constructor(private fb: FormBuilder, private alunoService: AlunoService) {
+
+    }
+
+    ngOnInit() {
+        this.aluno = new Aluno();
+        this.cursos = [
+            new Curso('angular2', 'Angular 2'),
+            new Curso('javaee', 'Java EE')
+        ];
+        this.buildForm(this.fb);
+
+        this.idEditar = -1;
+        this.alunos = this.alunoService.listarTodos();
+    }
+
+    constructor(fb: FormBuilder) {
+        this.aluno = new Aluno();
+        this.cursos = [
+           new Curso('angular2', 'Angular 2'),
+           new Curso('javaee', 'Java EE')
+        ]; 
+    	this.buildForm(fb);
+    }
+
     constructor() {
-          this.aluno = new Aluno();
-          this.cursos = [
-               new Curso('angular2', 'Angular 2'),
-               new Curso('javaee', 'Java EE')
-          ];    
+        this.aluno = new Aluno();
+        this.cursos = [
+           new Curso('angular2', 'Angular 2'),
+           new Curso('javaee', 'Java EE')
+        ];    
+    }
+
+    /*
+    enviar(): {
+	this.sucesso = true;
     }
     
     debug(): string {
-        return JSON.stringify("Dados do aluno");
+        return JSON.stringify(this.aluno);
+    }
+    */
+
+    buildForm(fb: FormBuilder): void {
+        this.alunoForm = fb.group({
+            nome:['',Validators.required],
+            email: ['',Validators.compose([Validators.required, EmailValidator.validate])],
+            curso: ['', Validators.required]
+        });
+    }
+
+    /*
+    hasErrors():boolean {
+        var hasErrors:boolean = false;
+
+        for(var controlName in this.alunoForm.controls) {
+            var control: AbstractControl = this.alunoForm.controls[controlName];
+            if(!control.valid && !control.pristine) {
+                hasErrors = true;
+                break;
+            }
+        }
+
+        return hasErrors;
+    }
+    */
+
+    cadastrar() {
+        this.alunoService.cadastrar(this.aluno);
+        this.aluno = new Aluno();        
+    }
+
+    editar(id: number) {
+        this.idEditar = id;
+        this.aluno = new Aluno(this.alunos[id].nome, this.alunos[id].email, this.alunos[id].idade, this.alunos[id].curso);
+    }
+
+    atualizar() {
+        this.alunoService.atualizar(this.idEditar, this.aluno);
+        this.aluno = new Aluno();
+        this.idEditar = -1;
+    }
+
+    excluir(id: number) {
+        this.alunoService.excluir(id);
+        this.idEditar = -1;
     }
 
 }
--- ProjetoAplicado/exemplo-angular2/app/js/form-aluno-component.js
+++ ProjetoAplicado/exemplo-angular2/app/js/form-aluno-component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './model/curso', './model/aluno'], function(exports_1) {
+System.register(['angular2/core', 'angular2/common', './validators/email-validator', './model/curso', './model/aluno', './service/aluno-service'], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,38 +8,100 @@ System.register(['angular2/core', './model/curso', './model/aluno'], function(ex
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, curso_1, aluno_1;
+    var core_1, common_1, email_validator_1, curso_1, aluno_1, aluno_service_1;
     var FormAlunoComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
             },
+            function (common_1_1) {
+                common_1 = common_1_1;
+            },
+            function (email_validator_1_1) {
+                email_validator_1 = email_validator_1_1;
+            },
             function (curso_1_1) {
                 curso_1 = curso_1_1;
             },
             function (aluno_1_1) {
                 aluno_1 = aluno_1_1;
+            },
+            function (aluno_service_1_1) {
+                aluno_service_1 = aluno_service_1_1;
             }],
         execute: function() {
             FormAlunoComponent = (function () {
-                function FormAlunoComponent() {
+                function FormAlunoComponent(fb, alunoService) {
+                    this.fb = fb;
+                    this.alunoService = alunoService;
                     this.sucesso = false;
+                }
+                FormAlunoComponent.prototype.ngOnInit = function () {
                     this.aluno = new aluno_1.Aluno();
                     this.cursos = [
                         new curso_1.Curso('angular2', 'Angular 2'),
                         new curso_1.Curso('javaee', 'Java EE')
                     ];
+                    this.buildForm(this.fb);
+                    this.idEditar = -1;
+                    this.alunos = this.alunoService.listarTodos();
+                };
+                /*
+                enviar(): {
+                this.sucesso = true;
                 }
-                FormAlunoComponent.prototype.debug = function () {
-                    return JSON.stringify("Dados do aluno");
+                
+                debug(): string {
+                    return JSON.stringify(this.aluno);
+                }
+                */
+                FormAlunoComponent.prototype.buildForm = function (fb) {
+                    this.alunoForm = fb.group({
+                        nome: ['', common_1.Validators.required],
+                        email: ['', common_1.Validators.compose([common_1.Validators.required, email_validator_1.EmailValidator.validate])],
+                        curso: ['', common_1.Validators.required]
+                    });
+                };
+                /*
+                hasErrors():boolean {
+                    var hasErrors:boolean = false;
+            
+                    for(var controlName in this.alunoForm.controls) {
+                        var control: AbstractControl = this.alunoForm.controls[controlName];
+                        if(!control.valid && !control.pristine) {
+                            hasErrors = true;
+                            break;
+                        }
+                    }
+            
+                    return hasErrors;
+                }
+                */
+                FormAlunoComponent.prototype.cadastrar = function () {
+                    this.alunoService.cadastrar(this.aluno);
+                    this.aluno = new aluno_1.Aluno();
+                };
+                FormAlunoComponent.prototype.editar = function (id) {
+                    this.idEditar = id;
+                    this.aluno = new aluno_1.Aluno(this.alunos[id].nome, this.alunos[id].email, this.alunos[id].idade, this.alunos[id].curso);
+                };
+                FormAlunoComponent.prototype.atualizar = function () {
+                    this.alunoService.atualizar(this.idEditar, this.aluno);
+                    this.aluno = new aluno_1.Aluno();
+                    this.idEditar = -1;
+                };
+                FormAlunoComponent.prototype.excluir = function (id) {
+                    this.alunoService.excluir(id);
+                    this.idEditar = -1;
                 };
                 FormAlunoComponent = __decorate([
                     core_1.Component({
                         selector: 'form-aluno',
-                        templateUrl: 'app/view/form-aluno.html'
+                        templateUrl: 'app/view/form-aluno.html',
+                        providers: [aluno_service_1.AlunoService]
                     }), 
-                    __metadata('design:paramtypes', [])
+                    __metadata('design:paramtypes', [common_1.FormBuilder, aluno_service_1.AlunoService])
                 ], FormAlunoComponent);
                 return FormAlunoComponent;
             })();
